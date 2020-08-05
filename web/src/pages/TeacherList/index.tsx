b2ab725b0db34f10450e1d0f5060d7ad92af459d
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-08-05 09:23:24 -0300

    feat(web): uses new Select component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React from 'react'
 
-import { PageHeader } from '../../components/PageHeader';
+import { PageHeader } from '../../components/PageHeader'
 
+import { TeacherItem } from '../../components/TeacherItem'
+import { Input } from '../../components/Input'
+import { Select } from '../../components/Select'
 
-import './styles.css';
-import { TeacherItem } from '../../components/TeacherItem';
-import { Input } from '../../components/Input';
+import './styles.css'
 
 export function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria" /> 
-          
-          <Input name="week_day" label="Dia da semana" /> 
-          
-          <Input type="time" name="time" label="Hora" /> 
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+            ]}
+          />
+
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
@@ -27,5 +58,5 @@ export function TeacherList() {
         <TeacherItem />
       </main>
     </div>
-  );
+  )
 }
