6463b200ce18930407925a3473fa17c8caa34283
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-08-05 08:46:56 -0300

    feat(web): add props description in PageHeader

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-import backIcon from '../../assets/images/icons/back.svg';
-import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
 import './styles.css'
 
 interface PageHeaderProps {
   title: string
+  description?: string
 }
 
-export const PageHeader : React.FC<PageHeaderProps> = ({title, children}) => {
+export const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  description,
+  children,
+}) => {
   return (
     <header className="page-header">
-    <div className="top-bar-container">
-      <Link to="/">
-        <img src={backIcon} alt="Voltar" />
-      </Link>
-      <img src={logoImg} alt="Proffy" />
-    </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
 
-    <div className="header-content">
-      <strong>{title}</strong>
+      <div className="header-content">
+        <strong>{title}</strong>
+        {description && <p>{description}</p>}
 
-      {children}
-    </div>
-  </header>
+        {children}
+      </div>
+    </header>
   )
 }
